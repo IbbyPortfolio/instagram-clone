aa9821ba5aca4f83abb6cf1fa1b1c9174107c800
Ibrahim Amin <ibrahim.s.amin@gmail.com>
2020-09-13 13:09:47 +1000

    post component completed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Post from "./Post";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [posts, setPosts] = useState([
+      {
+         username: "Ibby",
+         caption: "Component caption",
+         imageUrl:
+            "https://blog.addthiscdn.com/wp-content/uploads/2014/11/addthis-react-flux-javascript-scaling.png",
+      },
+      {
+         username: "Josh",
+         caption: "Josh caption",
+         imageUrl:
+            "https://blog.addthiscdn.com/wp-content/uploads/2014/11/addthis-react-flux-javascript-scaling.png",
+      },
+   ]);
+   
+   return (
+      <div className="app">
+         <div className="app__header">
+            <img
+               className="app__headerImage"
+               src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+               alt=""
+            />
+         </div>
+         {posts.map((post) => (
+            <Post
+               username={post.username}
+               caption={post.caption}
+               imageUrl={post.imageUrl}
+            />
+         ))}
+      </div>
+   );
 }
 
 export default App;
